41240b7ddc310fbcf1df09f2106434de720db338
Hyeok Jung Kim <hyeokjungkim@gmail.com>
2017-12-05 04:31:28 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,10 +2,8 @@
 def valid_move?(board, index)
   if index.to_i > 8
     return false
-  elsif position_taken?(board, index.to_i) == false
-    return false
   else
-    return true
+    return position_taken?(board, index.to_i) == true
   end
 end
 
